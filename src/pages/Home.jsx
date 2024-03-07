fbd63f8e2bd1f1e64d7bfc09fecfbf24190188e1
João Pedro Granelli <104399325+joaopedrogranelli@users.noreply.github.com>
2024-03-07 20:26:48 -0300

    wip

---
+++ src/pages/Home.jsx
@@ -0,0 +1,62 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
+
+{
+  /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+  POpPUP
+</div> */
+}
+const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+
+          <Bird />
+          <Sky />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
+          <Plane />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home";
+import Contact from "./Home";
+import About from "./Home";
+import Projects from "./Home";
+
+export { Home, About, Contact, Projects };
